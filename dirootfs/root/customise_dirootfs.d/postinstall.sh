e67c88dda5fd49aba9fb5d1985a03a60364c14d8
Jin Asanami <asanamij@ruritania.jp>
2026-03-10 22:05:47 +0900

    initial master commit

---
+++ dirootfs/root/customise_dirootfs.d/postinstall.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export PATH=/usr/bin:/usr/local/bin:/usr/sbin
+
+dpkg-reconfigure wine-stable-amd64 wine-stable-i386 wine-stable
---
+++ profiledef.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# (C) 2021-2026 Nakamura Musical Industries/NIPPON RURITANIA
+# License: 3-clause BSD
+DISTRO_NAME="Kamuriki Linux 4"
+DISTRO_UNAME="kamuriki"
+DISTRO_VERSION="4.2"
+UPSTREAM="debian"
+UPSTREAM_VERSION="trixie"
+MIRROR_URL="https://deb.debian.org/debian"
+ARCH="amd64"
+MULTIARCH=("i386")
