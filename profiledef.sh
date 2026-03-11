b9ff6332e891fd802cd51c23186319158773b23a
Jin Asanami <asanamij@ruritania.jp>
2026-03-11 21:52:00 +0900

    バージョン5に向けて色々開始

--- profiledef.sh
+++ profiledef.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # (C) 2021-2026 Nakamura Musical Industries/NIPPON RURITANIA
 # License: 3-clause BSD
-DISTRO_NAME="Kamuriki Linux 4"
+DISTRO_NAME="NMIMusic Erimakitokage"
 DISTRO_UNAME="kamuriki"
-DISTRO_VERSION="4.2"
+DISTRO_VERSION="5-alfa1"
 UPSTREAM="debian"
-UPSTREAM_VERSION="trixie"
+UPSTREAM_VERSION="forky"
 MIRROR_URL="https://deb.debian.org/debian"
 ARCH="amd64"
 MULTIARCH=("i386")
