7ed153e9a8074e3e3987fe6c1b220f39d2754c57
Jin Asanami <asanamij@ruritania.jp>
2026-08-22 13:07:29 +0900

    Toki回収に追随

--- profiledef.sh
+++ profiledef.sh
@@ -3,7 +3,7 @@
 # License: 3-clause BSD
 DISTRO_NAME="Kamuriki LGX 4"
 DISTRO_UNAME="kamuriki"
-DISTRO_VERSION="4.4"
+DISTRO_VERSION="4.5"
 UPSTREAM="debian"
 UPSTREAM_VERSION="trixie"
 MIRROR_URL="https://deb.debian.org/debian"
